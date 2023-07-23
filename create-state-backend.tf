1e7d5539cf333f41a08bcd63af84ee1e693ea934
Ciro Bessa <60109116+cirobessa@users.noreply.github.com>
2023-07-23 09:13:03 -0700

    Create create-state-backend.tf

---
+++ create-state-backend.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#terraform {
+#  backend "s3" {
+#    encrypt        = true
+#    bucket         = "name-of-your-s3-bucket"
+#    key            = "tfstate-s3-bucket"
+#    region         = "us-east-1"
+#    dynamodb_table = "terraform-state-lock-dynamo"
+#  }
+#}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "cirobessa-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
